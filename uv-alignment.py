8211490265517c1e1e2486e4c4cbb5d965d95960
Yuriki Le <3dyuriki@gmail.com>
2016-10-26 19:56:09 +0300

    v.01 made cleaning of code. Now it works only with 2 eyes

--- uv-alignment.py
+++ uv-alignment.py
@@ -1,3 +1,5 @@
+# v.01 we can align two eyes
+
 import bpy, bmesh
 from mathutils import Vector, Matrix
 from mathutils.geometry import intersect_ray_tri
@@ -7,28 +9,39 @@ from math import cos, sin
 
 # todo
 # 1.1. move timeslider to 336 frame for boy
-# 1.2. find screen coordinates of 3D eyes
-# 1.3. find intersections of ray from 3D eyes with BoyFotoPlane (in local coordinates of BoyFotoPlane)
+# 1.2. converting coordinates of vertices in center of 3D eyes from local to world space
 
-# 2.1. find distances: 1.between eyes; 2. between eyes' line and mouth
-# 2.2. scale uv horiz (to match distance between eyes). Anchor - left eye
-# 2.3. scale uv vertical (to match distance between eye's line and mouth). Anchor - left eye
+# 2.1. find coordinates of 3D eyes in pixelspace of screen
+# 2.2. find intersections of ray from 3D eyes with BoyFotoPlane (world space)
+# 2.3. converting intersection from world space to local space of BoyFotoPlane where axis X = x, Y = -z
+# 2.4. normalizing intersection and fixing Z axis (we need to do it because Z must look up)
 
-# 3.1. find angle between 3D eyes in screen coordinates (in pixels)
-# 3.2. find angle between eye's line and horizontal axis on the photo
-# 3.3. find difference between 1.1 and 1.2 and rotate uv for this value
-# 3.4. find new coordinates of eyes on photo (after uv rotation)
+# 3.1. getting coordinates of eyes on photo from FaceGen
+# 3.2. normalize coordinates of eyes on photo
 
-# 2.3. compare distances between 3d and photo for eyes and mouth
-# 6. move uv
+# 4.1. creating matrix of transformation
+# 4.2. transforming uv
 
 #######################################################################################
 ################################## 1.2 ################################################
 #######################################################################################
 
+#getting world coordinates of vertex 29478 in the center of left eye
+eyeL3D_wco = bpy.data.objects["BoyMorphed"].matrix_world*bpy.data.objects["BoyMorphed"].data.vertices[29478].co 
+#getting world coordinates of vertex 29783 in the center of right eye
+eyeR3D_wco = bpy.data.objects["BoyMorphed"].matrix_world*bpy.data.objects["BoyMorphed"].data.vertices[29783].co 
+
+
+
+
+#######################################################################################
+################################## 2.1 ################################################
+#######################################################################################
+
 scene = bpy.context.scene # getting current scene
 
-# looking for 3d view. It MUST be one with Camera as main camera
+# looking for 3d view. It MUST be one with Camera as main camera. 
+# Camera frame must be visible completely in 3d view
 def view3d_find():
     # returns first 3d view, normally we get from context
     for area in bpy.context.window.screen.areas:
@@ -40,38 +53,14 @@ def view3d_find():
                     return region, rv3d
     return None, None
 
-# looking for position of 4 points of camera borders in screen coordinates (in pixels).
-# 0,0 - left bottom corner of 3dview region.
-def view3d_camera_border(scene):
-    obj = scene.camera
-    cam = obj.data
-    
-    frame = cam.view_frame(scene)
-
-    # move into object space
-    frame = [obj.matrix_world * v for v in frame]
-
-    # move into pixelspace
-    region, rv3d = view3d_find()
-    frame_px = [location_3d_to_region_2d(region, rv3d, v) for v in frame]
-
-    return frame_px
-
-frame_px = view3d_camera_border(bpy.context.scene)
-
 region, rv3d = view3d_find()
-
-#getting world coordinates of vertex 29478 in the center of left eye
-eyeL3D_wco = bpy.data.objects["BoyMorphed"].matrix_world*bpy.data.objects["BoyMorphed"].data.vertices[29478].co 
-#getting world coordinates of vertex 29783 in the center of right eye
-eyeR3D_wco = bpy.data.objects["BoyMorphed"].matrix_world*bpy.data.objects["BoyMorphed"].data.vertices[29783].co 
 
 # looking for position of eyes and mouth in screeen coordinates (in pixels). 0,0 - left bottom corner of 3dview region
 eyeL3D_px = location_3d_to_region_2d(region, rv3d, eyeL3D_wco)
 eyeR3D_px = location_3d_to_region_2d(region, rv3d, eyeR3D_wco)
 
 #######################################################################################
-################################## 1.3 ################################################
+################################## 2.2 ################################################
 #######################################################################################
 
 # direction from camera origin to left eye
@@ -112,31 +101,45 @@ def get_intesection (ob, origin, ray_dir):
 interEyeL = get_intesection (BoyFotoPlane, origin, eyeL_ray_dir)
 interEyeR = get_intesection (BoyFotoPlane, origin, eyeR_ray_dir)
 
+#######################################################################################
+################################## 2.3 ################################################
+#######################################################################################
+
 # convert coordinates of intersection from world to FotoPlane's local
+# local coordinates of BoyFotoPlane start in center and have axis X = looking right, Y = 0, Z = looking down
 interEyeL_local = BoyFotoPlane.matrix_world.inverted()*interEyeL
 interEyeR_local = BoyFotoPlane.matrix_world.inverted()*interEyeR
 
+#######################################################################################
+################################## 2.4 ################################################
+#######################################################################################
+
 photoPlaneSize = 4.71391 # half width/height of the BoyFotoPlane in units (because width=height)
 
-# normalizing coordinates of left eye intersection from local to range 0,0-1,1
+# normalizing coordinates of left eye intersection from local to range 0,0-1,1 and inverting Z axis
 interEyeL_co_nx = ((interEyeL_local[0] + photoPlaneSize)/(photoPlaneSize))/2
-interEyeL_co_nz = ((interEyeL_local[2] + photoPlaneSize)/(photoPlaneSize))/2
+interEyeL_co_nz = 1 - ((interEyeL_local[2] + photoPlaneSize)/(photoPlaneSize))/2
 
-# normalizing coordinates of right eye intersection from local to range 0,0-1,1
+# normalizing coordinates of right eye intersection from local to range 0,0-1,1 and inverting Z axis
 interEyeR_co_nx = ((interEyeR_local[0] + photoPlaneSize)/(photoPlaneSize))/2
-interEyeR_co_nz = ((interEyeR_local[2] + photoPlaneSize)/(photoPlaneSize))/2
+interEyeR_co_nz = 1- ((interEyeR_local[2] + photoPlaneSize)/(photoPlaneSize))/2
 
 print ("Right Eye Plane =", interEyeR_co_nx, interEyeR_co_nz)
 print ("Left Eye Plane =", interEyeL_co_nx, interEyeL_co_nz)
 
+
+
+
 #######################################################################################
-################################## 3.2 ################################################
+################################## 3.1 ################################################
 #######################################################################################
+
+# getting coordinates of eyes on photo from FaceGen
 
 # x=0, z=0 are in the center of the photo.
 # z looking down.
 # I named vertical axis as z because
-# when I'm using local coordinates of FotoPlane I have axis x and z (y = 0 for all vertices).
+# when I'm using local coordinates of BoyFotoPlane I have axis x and z (y = 0 for all vertices).
 eyeL_photo_x = 89 # x position of the left eye on the photo in pixels
 eyeL_photo_z = -219 # z position of the left eye on the photo in pixels
 
@@ -148,76 +151,32 @@ photo_z = 512 # half height of the photo in pixels
 
 ar = 1.44151 # aspect ratio of BoyFotoPlane
 
-# normalizing coordinates of left eye on the photo from pixels to 0,0-1,1
-eyeL_foto_co_nx = ((eyeL_photo_x + photo_x)/(photo_x))/2
-eyeL_foto_co_nz = ((eyeL_photo_z + photo_z)/(photo_z))/2
-
-# normalizing coordinates of right eye on the photo from pixels to 0,0-1,1
-eyeR_foto_co_nx = ((eyeR_photo_x + photo_x)/(photo_x))/2
-eyeR_foto_co_nz = ((eyeR_photo_z + photo_z)/(photo_z))/2
-
-print ("Right Eye Foto =", eyeR_foto_co_nx, eyeR_foto_co_nz)
-print ("Left Eye Foto =", eyeL_foto_co_nx, eyeL_foto_co_nz)
-
 #######################################################################################
 ################################## 3.2 ################################################
 #######################################################################################
 
-# distance between intersections of 3D eyes on BoyFotoPlane 
-dist_btw_interEyes = (Vector((interEyeL_co_nx, interEyeL_co_nz)) - Vector((interEyeR_co_nx, interEyeR_co_nz))).length
-#print ("dist_btw_interEyes =", dist_btw_interEyes)
+# normalizing coordinates of left eye on the photo from pixels to 0,0-1,1 and inverting Z axis
+eyeL_foto_co_nx = ((eyeL_photo_x + photo_x)/(photo_x))/2
+eyeL_foto_co_nz = 1 - ((eyeL_photo_z + photo_z)/(photo_z))/2
 
-# distance between eyes on BoyFotoPlane
-# TODO
-# MAYBE I need to take into account real scale of foto (because uv is square but foto is rectangle)
-dist_btw_eyes_foto = (Vector((eyeL_foto_co_nx, eyeL_foto_co_nz)) - Vector((eyeR_foto_co_nx, eyeR_foto_co_nz))).length
-#print ("dist_btw_eyes_foto =", dist_btw_eyes_foto)
+# normalizing coordinates of right eye on the photo from pixels to 0,0-1,1 and inverting Z axis
+eyeR_foto_co_nx = ((eyeR_photo_x + photo_x)/(photo_x))/2
+eyeR_foto_co_nz = 1 - ((eyeR_photo_z + photo_z)/(photo_z))/2
 
-# difference between eyes' distances (between photo and 3D)
-diff_eyes_dist = dist_btw_interEyes - dist_btw_eyes_foto + 1
-#print ("diff_eyes_dist =", diff_eyes_dist)
-
-#######################################################################################
-################################## 1.2 ################################################
-#######################################################################################
-
-# looking for tilt of 3D eyes in screen coordinates
-eyes3D_vec = eyeL3D_px - eyeR3D_px
-eyes3D_angle =  eyes3D_vec.angle_signed (((1, 0)), None)
-#print ("eyes3D_angle =", eyes3D_angle)
+print ("Right Eye Foto =", eyeR_foto_co_nx, eyeR_foto_co_nz)
+print ("Left Eye Foto =", eyeL_foto_co_nx, eyeL_foto_co_nz)
 
 
 
-# looking for tilt of eyes on photo: angle between eyes' line and x axis
-eyes_vec = Vector ((eyeL_photo_x, eyeL_photo_z)) - Vector ((eyeR_photo_x, eyeR_photo_z))
-eyes_angle =  eyes_vec.angle_signed (((1, 0)), None)
-#print ("eyes_angle =", eyes_angle)
-
-
-# substracting tilt of 3D eyes from tilt of eyes on photo
-eyes_angle_dif = eyes_angle + eyes3D_angle
 
 #######################################################################################
 ################################## 4.1 ################################################
 #######################################################################################
 
-# Translation
-
-# difference between intersection and real left eye location on photo
-diff_x = interEyeL_co_nx - eyeL_foto_co_nx
-diff_z = interEyeL_co_nz - eyeL_foto_co_nz
-#print (diff_x)
-#print (diff_z)
-
+# creating matrix 3x3 in this strange way (because I don't know how to do it in oter way)
 matrixT = Matrix.Translation ((1,2,3)).to_3x3 ()
-'''matrixT[0][0] = diff_eyes_dist*cos(eyes_angle_dif)
-matrixT[0][1] = -diff_eyes_dist*sin(eyes_angle_dif)
-matrixT[0][2] = diff_x
 
-matrixT[1][0] = diff_eyes_dist*sin(eyes_angle_dif)
-matrixT[1][1] = diff_eyes_dist*cos(eyes_angle_dif)
-matrixT[1][2] = diff_z'''
-
+# filling matrix with values calculated with help of this https://github.com/axelpale/nudged-py
 matrixT[0][0] = 1.320802769303709
 matrixT[0][1] = -0.15099206295327702
 matrixT[0][2] = -0.17697950213490118
@@ -229,12 +188,13 @@ matrixT[1][2] = -0.0895266793498636
 matrixT[2][0] = 0
 matrixT[2][1] = 0
 matrixT[2][2] = 1
-
-
-
 #print (matrixT)
 
-# scaling uv
+#######################################################################################
+################################## 4.2 ################################################
+#######################################################################################
+
+# transforming uv
 UVmap = BoyFotoPlane.data.uv_layers.active
 for v in BoyFotoPlane.data.loops :
     uv_vec = UVmap.data[v.index].uv # uv vector
@@ -243,9 +203,3 @@ for v in BoyFotoPlane.data.loops :
 
     uv_vec[0] = uv_tr[0]
     uv_vec[1] = uv_tr[1]
-
-#######################################################################################
-################################## 5.1 ################################################
-#######################################################################################
-
-
