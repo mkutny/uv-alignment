d4283e4f421691e4d414e73062359c07c72542d6
Max Kutny <mkutny@gmail.com>
2016-11-08 23:09:17 +0200

    now comaring all UVs

--- trans_mat.py
+++ trans_mat.py
@@ -47,32 +47,30 @@ def test_144_vs_069():
   r_plane = [0.3767, 0.5339]
   l_plane = [0.6617, 0.5332]
 
-  r_foto = [0.2539, 0.5762/foto_y_to_x] # 0.82
-  l_foto = [0.4104, 0.5762/foto_y_to_x]
+  r_foto = [0.2539*foto_y_to_x, (0.5762*plane_y_to_x)] # 0.82
+  l_foto = [0.4104*foto_y_to_x, (0.5762*plane_y_to_x)]
 
   print(l_foto)
 
   trans_mat = get_transform(l_plane, r_plane, l_foto, r_foto)
 
-  uv_vec = [[1], # (1,1)
-            [1],
-            [1]]
+  uv_vec = [[0, 1, 1, 0], # (1,1)
+            [0, 0, 1, 1],
+            [1, 1, 1, 1]]
   np_uv_vec = np.matrix(uv_vec)
 
-  uv = trans_mat * np_uv_vec
-
+  uv_t = trans_mat * np_uv_vec
+  uv = uv_t.T
   print(uv)
 
-  gt_uv = [[0.0477, 0.0001], # (0,0)
-           [0.5970, 0.0028], # (1,0)
-           [0.5956, 1.0838], # (1,1)
-           [0.0463, 1.0811]] # (0,1)
+  gt_uv = [[0.0477, 0.0001, 1], # (0,0)
+           [0.5970, 0.0028, 1], # (1,0)
+           [0.5956, 1.0838, 1], # (1,1)
+           [0.0463, 1.0811, 1]] # (0,1)
 
-  #assert round(uv.item(0), 3) == round(gt_uv[2][0], 3) # x of (0,0)
-  #assert round(uv.item(1), 3) == round(gt_uv[2][1], 3) # y of (0,0)
-  
-  assert round(uv.item(0), 3) == round(gt_uv[2][0], 3)
-  assert round(uv.item(1), 3) == round(gt_uv[2][1], 3)
+  assert np.allclose(uv, np.matrix(gt_uv), 1e-03)
+  #assert round(uv.item(0), 3) == round(gt_uv[2][0], 3)
+  #assert round(uv.item(1), 3) == round(gt_uv[2][1], 3)
 
 
 
@@ -92,19 +90,23 @@ def test_144_vs_144():
   trans_mat = get_transform(l_plane, r_plane, l_foto, r_foto)
   #trans_mat = get_transform(l_foto, r_foto, l_plane, r_plane)
 
-  uv_vec = [[1], # (1,1)
+  uv_mat = [[0, 1, 1, 0], # (1,1)
+            [0, 0, 1, 1],
+            [1, 1, 1, 1]]
+  np_uv_mat = np.matrix(uv_mat)
+  uv_mat_t = trans_mat * np_uv_mat
+  print(uv_mat_t.T)
+
+  uv_vec = [[1],
             [1],
             [1]]
   np_uv_vec = np.matrix(uv_vec)
+  uv_vec_t = trans_mat * np_uv_vec
+  print(uv_vec_t)
 
-  uv = trans_mat * np_uv_vec
+  gt_uv = [[-0.038, -0.033, 1], # (0,0)
+           [ 1.101, -0.03,  1 ], # (1,0)
+           [ 1.098,  1.109, 1], # (1,1)
+           [-0.04,   1.106, 1]] # (0,1)
 
-  print(uv)
-
-  gt_uv = [[-0.038, -0.033], # (0,0)
-           [ 1.101, -0.03 ], # (1,0)
-           [ 1.098,  1.109], # (1,1)
-           [-0.04,   1.106]] # (0,1)
-
-  assert round(uv.item(0), 3) == round(gt_uv[2][0], 3)
-  assert round(uv.item(1), 3) == round(gt_uv[2][1], 3)
+  assert np.allclose(uv_mat_t.T, np.matrix(gt_uv), 1e-03, 1e-03)
