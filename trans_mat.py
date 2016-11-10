5baada090ffed45dfc64c029eb3e5c636aa6b0c9
Max Kutny <mkutny@gmail.com>
2016-11-10 11:40:23 +0200

    p, p, f, p

--- trans_mat.py
+++ trans_mat.py
@@ -2,6 +2,7 @@
 # run with: 'py.test -s trans_mat.py'
 
 import numpy as np
+import pytest
 
 def get_transform(src2, src1, targ2, targ1):
   plane_mat = np.matrix([[src1[0], -src1[1], 1, 0],
@@ -33,13 +34,6 @@ def get_transform(src2, src1, targ2, targ1):
 
 
 def get_rescale(photo_y_scale):
-  plane_y_scale = 1.44
-  plane_scale_mat = np.matrix([[1, 0,             0],
-                               [0, plane_y_scale, 0],
-                               [0, 0,             1]])
-  photo_scale_mat = np.matrix([[1, 0,             0],
-                               [0, 1/photo_y_scale, 0],
-                               [0, 0,             1]])
   rescale_mat = np.matrix([[1,          0,                0],
                            [0,          1/photo_y_scale,  0],
                            [0,          0,                1]])
@@ -50,39 +44,7 @@ def get_rescale(photo_y_scale):
 #########################################################
 #########################################################
 #########################################################
-def test_144_vs_069():
-  plane_y_scale = 1.44 # not used
 
-  photo_x_size = 2127
-  photo_y_size = 1477
-  photo_y_scale = photo_y_size/photo_x_size # 0.6944
-
-  r_plane = [0.3767, 0.5339*plane_y_scale]
-  l_plane = [0.6617, 0.5332*plane_y_scale]
-
-  r_foto = [540/photo_x_size, 851/photo_y_size*photo_y_scale]
-  l_foto = [873/photo_x_size, 851/photo_y_size*photo_y_scale]
-
-  trans_mat, scale, rotation, translation = get_transform(l_plane, r_plane, l_foto, r_foto)
-  #print("scale:\n", scale)
-  #print("rotation:\n", rotation)
-  #print("translation:\n", translation)
-  #print("transformation:\n", trans_mat)
-
-  uv_vec = np.matrix([[0, 1, 1, 0], # (1,1)
-                      [0, 0, 1.44, 1.44],
-                      [1, 1, 1, 1]])
-  scale_back_mat = get_rescale(photo_y_scale)
-
-  uv_mat_t = scale_back_mat * trans_mat * uv_vec
-  #print("original transform 1.44 vs 0.69:\n", uv_mat_t.T)
-
-  gt_uv = [[0.0477, 0.0001, 1], # (0,0)
-           [0.5970, 0.0028, 1], # (1,0)
-           [0.5956, 1.0838, 1], # (1,1)
-           [0.0463, 1.0811, 1]] # (0,1)
-
-  assert np.allclose(uv_mat_t.T, np.matrix(gt_uv), 1e-01, 1e-01)
 
 
 
@@ -122,25 +84,90 @@ def test_144_vs_144():
 
 
 
-###################
-def test_144_vs_144_45deg():
 
-  plane_y_scale = 1.44
 
-  photo_x_size = 764
-  photo_y_size = 1110
-  photo_y_scale = photo_y_size/photo_x_size # 1.44
 
+def test_144_vs_069():
+  plane_y_scale = 1.44 # not used
+
+  photo_x_size = 2127
+  photo_y_size = 1477
+  photo_y_scale = photo_y_size/photo_x_size # 0.6944
 
   r_plane = [0.3767, 0.5339*plane_y_scale]
   l_plane = [0.6617, 0.5332*plane_y_scale]
 
-  r_foto_abs = [345, photo_y_size - 490]
-  l_foto_abs = [559, photo_y_size - 294]
+  r_foto = [540/photo_x_size, 851/photo_y_size*photo_y_scale]
+  l_foto = [873/photo_x_size, 851/photo_y_size*photo_y_scale]
 
-  r_foto = [345/photo_x_size, (photo_y_size - 490)/photo_y_size*photo_y_scale]
-  l_foto = [559/photo_x_size, (photo_y_size - 294)/photo_y_size*photo_y_scale]
+  trans_mat, scale, rotation, translation = get_transform(l_plane, r_plane, l_foto, r_foto)
+  #print("scale:\n", scale)
+  #print("rotation:\n", rotation)
+  #print("translation:\n", translation)
+  #print("transformation:\n", trans_mat)
 
+  uv_vec = np.matrix([[0, 1, 1, 0], # (1,1)
+                      [0, 0, 1.44, 1.44],
+                      [1, 1, 1, 1]])
+  scale_back_mat = get_rescale(photo_y_scale)
+
+  uv_mat_t = scale_back_mat * trans_mat * uv_vec
+  #print("original transform 1.44 vs 0.69:\n", uv_mat_t.T)
+
+  gt_uv = [[0.0477, 0.0001, 1], # (0,0)
+           [0.5970, 0.0028, 1], # (1,0)
+           [0.5956, 1.0838, 1], # (1,1)
+           [0.0463, 1.0811, 1]] # (0,1)
+
+  assert np.allclose(uv_mat_t.T, np.matrix(gt_uv), 1e-01, 1e-01)
+
+
+
+
+
+@pytest.fixture(scope="function", params=[
+(# plane y/x: 1.44, photo y/x: 0.6944, rotation: 0
+ 2127, # photo_x_size
+ 1477, # photo_y_size
+ [540, 851], # photo right x,y
+ [873, 851], # photo left x,y
+ # (expected result (ground truth)
+ [[0.0477, 0.0001, 1], # (0,0)
+  [0.5970, 0.0028, 1], # (1,0)
+  [0.5956, 1.0838, 1], # (1,1)
+  [0.0463, 1.0811, 1]] # (0,1)
+),
+
+(# plane y/x: 1.44, photo y/x: 1.44, rotation: 45deg CCW
+ 764, # photo_x_size
+ 1110, # photo_y_size
+ [345, 764-490], # photo right x,y
+ [559, 1100-294], # photo left x,y
+ # (expected result (ground truth)
+ [[ 0.781, -0.217, 1], # (0,0)
+  [ 1.777,  0.423, 1], # (1,0)
+  [ 0.443,  1.414, 1], # (1,1)
+  [-0.552,  0.774, 1]] # (0,1)),
+)
+],
+ids=["1.44 vs 0.69, rotation 0", "144_vs_144_45deg"]
+)
+def prepare_fixture(request):
+  plane_y_scale = 1.44
+  r_plane = [0.3767, 0.5339*plane_y_scale]
+  l_plane = [0.6617, 0.5332*plane_y_scale]
+  return (plane_y_scale, r_plane, l_plane) + request.param
+
+
+def test_alignment(prepare_fixture):
+  # setting initial parameters
+  (plane_y_scale, r_plane, l_plane, photo_x_size, photo_y_size, r_photo_abs, l_photo_abs, gt_uv) = prepare_fixture
+
+  photo_y_scale = photo_y_size/photo_x_size
+
+  r_foto = [r_photo_abs[0]/photo_x_size, (photo_y_size - 490)/photo_x_size]
+  l_foto = [l_photo_abs[0]/photo_x_size, (photo_y_size - 294)/photo_x_size]
+  
   trans_mat, scale, rotation, translation = get_transform(l_plane, r_plane, l_foto, r_foto)
   #print("scale:\n", scale)
   #print("rotation:\n", rotation)
@@ -156,28 +183,4 @@ def test_144_vs_144_45deg():
   uv_mat_t = scale_back_mat * trans_mat * uv_mat
 
   #print("1.44 vs 1.44:\n", uv_mat_t.T)
-
-  gt_uv = [[ 0.781, -0.217, 1], # (0,0)
-           [ 1.777,  0.423, 1], # (1,0)
-           [ 0.443,  1.414, 1], # (1,1)
-           [-0.552,  0.774, 1]] # (0,1)
-
   assert np.allclose(uv_mat_t.T, np.matrix(gt_uv), 1e-01, 1e-01)
-
-
-
-#### solving 1.44:1.44
-#solve = get_transform([0, 0], [1, 1], [-0.038, -0.033], [1.098,  1.109])
-#print("solving 1.44:1.44 for (0,0) & (1,1):\n", solve, "\n")
-
-#solve = get_transform([1, 0], [0, 1], [1.101, -0.030], [-0.040,  1.106])
-#print("solving 1.44:1.44 for (1,0) & (0,1):\n", solve, "\n")
-#print()
-
-# solving 1.44:0.69
-#solve = get_transform([0, 0], [1, 1], [0.0477, 0.0001], [0.5956, 1.0838])
-#print("solving 1.44:0.69 for (0,0) & (1,1):\n", solve, "\n")
-
-#solve = get_transform([1, 0], [0, 1], [0.5970, 0.0028], [0.0463, 1.0811])
-#print("solving 1.44:0.69 for (1,0) & (0,1):\n", solve, "\n")
-
